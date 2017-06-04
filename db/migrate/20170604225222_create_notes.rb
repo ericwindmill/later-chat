1891862928273e1e6f452cbde2c1b66ef6a4ea54
Monte Jiran <montejiran@gmail.com>
2017-06-04 16:01:27 -0700

    note migration

--- db/migrate/20170604225222_create_notes.rb
+++ db/migrate/20170604225222_create_notes.rb
@@ -1,8 +1,12 @@
 class CreateNotes < ActiveRecord::Migration[5.0]
   def change
     create_table :notes do |t|
-
+      t.integer :post_id, null: false
+      t.integer :recipient_id, null: false
+      t.boolean :read_status, default: false
       t.timestamps
     end
+    add_index :notes, :post_id
+    add_index :notes, :recipient_id
   end
 end
