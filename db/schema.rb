281c1bc1bf312c6dfc625691eb264018b6521379
Monte Jiran <montejiran@gmail.com>
2017-06-04 16:05:33 -0700

    migrate tables

---
+++ db/schema.rb
@@ -0,0 +1,55 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170604225226) do
+
+  create_table "follows", force: :cascade do |t|
+    t.integer  "follower_id", null: false
+    t.integer  "leader_id",   null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
+    t.index ["leader_id"], name: "index_follows_on_leader_id"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.integer  "post_id",                      null: false
+    t.integer  "recipient_id",                 null: false
+    t.boolean  "read_status",  default: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.index ["post_id"], name: "index_notes_on_post_id"
+    t.index ["recipient_id"], name: "index_notes_on_recipient_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "body",                       null: false
+    t.string   "location",                   null: false
+    t.string   "image_url"
+    t.boolean  "public",     default: false
+    t.integer  "author_id"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.index ["location"], name: "index_posts_on_location"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",        null: false
+    t.string   "password_digest", null: false
+    t.string   "session_token",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+end
