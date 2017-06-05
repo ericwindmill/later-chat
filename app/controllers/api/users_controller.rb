b84df4f13600cb60629659683b6dccd80ab50c69
Monte Jiran <montejiran@gmail.com>
2017-06-04 17:11:11 -0700

    add partial posts controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,9 @@
 class Api::UsersController < ApplicationController
 
+  def index
+    @users = User.limit(10).where(["lower(username) LIKE ?", "%#{params[:searchStr].downcase}%"])
+    render :index
+  end
 
 	def create
 		@user = User.new(user_params)
@@ -10,7 +14,7 @@ class Api::UsersController < ApplicationController
 		else
 			render json: @user.errors.full_messages, status: 422
 		end
-    
+
 	end
 
 	private
