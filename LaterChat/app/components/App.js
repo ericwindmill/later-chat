61ddf1b8c096ed5e0266342d0fe8f97ac28887ab
Monte Jiran <montejiran@gmail.com>
2017-06-05 16:19:41 -0700

    add redux react-redux redux-thunk to package, create store, root_reducer

--- LaterChat/app/components/App.js
+++ LaterChat/app/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import configureStore from '../store/store';
 import {
   StyleSheet,
   Text,
@@ -7,6 +8,7 @@ import {
 import { Tabs } from './config/router';
 
 export default class App extends Component {
+
   render() {
     return (
         <Tabs />
