5e86429d8f202de43242dbf18cf2e1a04db1ea93
Monte Jiran <montejiran@gmail.com>
2017-06-06 11:51:57 -0700

    add set token to async storage

--- app/util/session_api_util.js
+++ app/util/session_api_util.js
@@ -1,3 +1,5 @@
+import ASYNC from './async_util.js';
+
 export const login = user => {
   return fetch('http://localhost:3000/api/session', {
     method: 'POST',
@@ -8,7 +10,8 @@ export const login = user => {
     }
   })
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => ASYNC.setItem('token', data.auth_token))
+    .then( () => ASYNC.getItem('token'))
     .catch(error => console.log(error));
 };
 
