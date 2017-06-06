f9bbaf663d69ca62bbb4f89d84b9d58fc6aa1728
npl22 <nli22@jhu.edu>
2017-06-06 11:07:23 -0700

    Remove console.log for store

--- app/reducers/session_reducer.js
+++ app/reducers/session_reducer.js
@@ -14,6 +14,7 @@ const SessionReducer = (state = nullUser, action) => {
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_CURRENT_USER:
+      console.log("inside reducer");
       const currentUser = action.currentUser;
       return merge({}, nullUser, {
         currentUser
