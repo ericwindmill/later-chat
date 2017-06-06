0c92a7192e885de05e202d87258f9aea483a5bff
npl22 <nli22@jhu.edu>
2017-06-06 11:02:57 -0700

    Add first draft of login container, map login action to login form

---
+++ app/components/screens/auth/login_container.js
@@ -0,0 +1,20 @@
+import { connect } from 'redux';
+
+import { login, signup, logout } from '../../../actions/session_actions';
+import LogIn from './login';
+
+const mapStateToProps = ({ session }) => {
+  return {
+    loggedIn: Boolean(session.currentUser),
+    errors: session.errors
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    login: user => dispatch(login(user)),
+    signup: user => dispatch(signup(user))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LogIn);
