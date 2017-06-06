a8a4db9acf64963de24c2aaa433e90ae823a4181
Eric Windmill <ericwindmill@gmail.com>
2017-06-06 09:28:28 -0700

    Fix fetchAllPosts

--- app/util/posts_api_util.js
+++ app/util/posts_api_util.js
@@ -2,10 +2,10 @@
 export const fetchAllPosts = () => {
   fetch('http://localhost:3000/api/posts', {
     headers: {
-      // 'Accept': 'application/json'
+      'Accept': 'application/json'
     }
   })
     .then(res => res.json())
     .then(data => console.log(data))
-    // .catch(error => console.log(error))
+    .catch(error => console.log(error))
 }
