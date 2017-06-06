3cf586ccf8ee907d5af49c27d3eed16290532320
npl22 <nli22@jhu.edu>
2017-06-06 11:03:42 -0700

    Remove unecessary argument in logout action

--- app/actions/session_actions.js
+++ app/actions/session_actions.js
@@ -14,23 +14,23 @@ export const receiveErrors = errors => ({
 });
 
 export const signup = user => dispatch => (
-  APIUtil.signup(user).then(user => (
-    dispatch(receiveCurrentUser(user))
+  APIUtil.signup(user).then(userResponse => (
+    dispatch(receiveCurrentUser(userResponse))
   ), err => (
     dispatch(receiveErrors(err.responseJSON))
   ))
 );
 
 export const login = user => dispatch => (
-  APIUtil.login(user).then(user => (
-    dispatch(receiveCurrentUser(user))
+  APIUtil.login(user).then(userResponse => (
+    dispatch(receiveCurrentUser(userResponse))
   ), err => (
     dispatch(receiveErrors(err.responseJSON))
   ))
 );
 
 export const logout = () => dispatch => (
-  APIUtil.logout().then(user => (
+  APIUtil.logout().then(() => (
     dispatch(receiveCurrentUser(null))
   ))
 );
