ad681b7a79a25ffc7ca61354c583c0d459401550
Monte Jiran <montejiran@gmail.com>
2017-06-06 21:08:46 -0700

    add location fetch to app will mount

--- app/components/App.js
+++ app/components/App.js
@@ -10,11 +10,38 @@ import  LogIn from './screens/auth/login';
 import { Provider } from 'react-redux';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialPosition: 'unknown',
+      lastPosition: 'unknown',
+    };
+    this.store = configureStore();
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var initialPosition = position;
+        this.setState({initialPosition});
+      },
+      (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lastPosition = position;
+      this.setState({lastPosition});
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
 
   render () {
-    const store = configureStore();
+    console.log(this.state);
     return (
-      <Provider store={store}>
+      <Provider store={this.store}>
         <AuthNav />
       </Provider>
     );
