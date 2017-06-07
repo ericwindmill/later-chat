739ef053156bebf5667ff920afa022cda7afcf64
Monte Jiran <montejiran@gmail.com>
2017-06-07 16:43:46 -0700

    checkout

--- app/components/screens/auth/splash.js
+++ app/components/screens/auth/splash.js
@@ -28,10 +28,7 @@ export default class SplashScreen extends Component {
     async verifyToken(token) {
       let accessToken = token
       try {
-<<<<<<< HEAD
-=======
         // let response = await fetch('http://later-chat.herokuapp/api/verify?session%5Baccess_token%5D='+accessToken);
->>>>>>> 0ca5907f0ffcba7a4e057ed25a39085ea024602d
         let response = await fetch('http://localhost:3000/api/verify?session%5Baccess_token%5D='+accessToken);
         let res = await response.text();
         if (response.status >= 200 && response.status < 300) {
