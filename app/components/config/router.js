86ef3969e6f304d0b188c89d511febd9ed152fcf
Monte Jiran <montejiran@gmail.com>
2017-06-07 11:04:14 -0700

    add loction to notifications for testing

--- app/components/config/router.js
+++ app/components/config/router.js
@@ -8,7 +8,6 @@ import HomeFeed from '../screens/feed/home_feed';
 import PublicFeed from '../screens/feed/public_feed';
 import NewPost from '../screens/new_post/new_post';
 import MapScreen from '../screens/map/map';
-import Notifications from '../screens/notifications/notifications';
 import LogOutContainer from '../screens/auth/logout_container';
 // Stack Screens
 import LogIn from '../screens/auth/login';
@@ -16,6 +15,7 @@ import SignUp from '../screens/auth/signup';
 import SelectRecipients from '../screens/new_post/select_recipients';
 import LogInContainer from '../screens/auth/login_container';
 import SplashScreenContainer from '../screens/auth/splash_container';
+import NotificationsContainer from '../screens/notifications/notifications_container';
 
 export const newPostNav = StackNavigator({
   'Add Post': {
@@ -45,7 +45,7 @@ export const Tabs = TabNavigator({
     screen: MapScreen
   },
   Notifications: {
-    screen: Notifications
+    screen: NotificationsContainer
   },
   LogOut: {
     screen: LogOutContainer
@@ -69,4 +69,3 @@ export const AuthNav = StackNavigator({
     screen: SignUp
   }
 });
-
