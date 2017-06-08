7fdc6d0f54103ac46e499cc8be35e15717c52c0a
Monte Jiran <montejiran@gmail.com>
2017-06-08 11:08:21 -0700

    add search users reducer and to root reducer

--- app/reducers/root_reducer.js
+++ app/reducers/root_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import PostsReducer from './posts_reducer';
 import LocationReducer from './location_reducer';
+import SearchUsersReducer from './search_users_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,
   posts: PostsReducer,
-  location: LocationReducer
+  location: LocationReducer,
+  searchUsersResults: SearchUsersReducer 
 });
 
 export default RootReducer;
