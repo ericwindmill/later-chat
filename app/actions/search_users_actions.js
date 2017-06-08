55d8bc1177f27f9a5370467c48244b45a1068112
Monte Jiran <montejiran@gmail.com>
2017-06-08 11:04:41 -0700

    add search actions

---
+++ app/actions/search_users_actions.js
@@ -0,0 +1,17 @@
+import * as APIUtil from '../util/search_users_api_util';
+
+export const RECEIVE_SEARCH_RESULTS = "RECEIVE_SEARCH_RESULTS";
+export const CLEAR_SEARCH_RESULTS = "CLEAR_SEARCH_RESULTS";
+
+export const requestSearch = searchStr => dispatch => {
+  return APIUtil.fetchSearchUsers(searchStr).then(results => dispatch(receiveSearchResults(results)));
+};
+
+export const receiveSearchResults = searchResults => ({
+  type: RECEIVE_SEARCH_RESULTS,
+  searchResults
+});
+
+export const clearSearchResults = () => ({
+  type: CLEAR_SEARCH_RESULTS,
+});
