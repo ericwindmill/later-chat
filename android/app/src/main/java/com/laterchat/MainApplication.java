46438e3d4169e52c6299ce50602aa8f47c7202cd
Monte Jiran <montejiran@gmail.com>
2017-06-07 21:22:50 -0700

    fetch basic google places data

--- android/app/src/main/java/com/laterchat/MainApplication.java
+++ android/app/src/main/java/com/laterchat/MainApplication.java
@@ -3,6 +3,7 @@ package com.laterchat;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGooglePlacesPackage(),
             new RCTCameraPackage(),
             new VectorIconsPackage()
       );
