27d0f82d60df7a5a06570e8fe6b20deab6691630
Monte Jiran <montejiran@gmail.com>
2017-06-08 10:49:26 -0700

    add input to notifications page

--- app/components/screens/notifications/notifications.js
+++ app/components/screens/notifications/notifications.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import baseStyles from '../styles/styles'
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableOpacity
 } from 'react-native';
 
 export default class Notifications extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchStr: ""
+    };
   }
 
   render() {
@@ -17,7 +24,25 @@ export default class Notifications extends Component {
         <Text>
           Notifications
         </Text>
+        <View>
+          <TextInput
+            style={baseStyles.input}
+            placeholder='Search Users'
+            onChangeText={(text) => this.setState({searchStr: text})}
+          />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 60,
+    justifyContent: 'flex-end'
+  },
+  inputContainer: {
+
+  }
+});
