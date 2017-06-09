c4a79bea34b7986378b8d061eb59b0c20bb5b8e7
Monte Jiran <montejiran@gmail.com>
2017-06-09 16:48:39 -0700

    change empty string to regex

--- app/util/notes_api_util.js
+++ app/util/notes_api_util.js
@@ -1,10 +1,10 @@
 export const fetchAllNotes = (user_id, locations) => {
-  locations = locations.map(loc => loc.replace(" ", "%20")).join('&locations[]=')
+  locations = locations.map(loc => loc.replace(/ /g, "%20")).join('&locations[]=');
   fetch('https://later-chat.herokuapp.com/api/posts?type=note&recipient_id=' + user_id + '&locations[]=' + locations, {
     headers: {
       'Accept': 'application/json'
     }
   })
     .then(res => res.json())
-    .catch(error => console.log(error))
-}
+    .catch(error => console.log(error));
+};
