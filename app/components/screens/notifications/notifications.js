fc47f9b020a5be73288318d9eee8bdb785b28b87
Monte Jiran <montejiran@gmail.com>
2017-06-10 11:41:30 -0700

    remove search from notifications

--- app/components/screens/notifications/notifications.js
+++ app/components/screens/notifications/notifications.js
@@ -13,13 +13,6 @@ import {
 export default class Notifications extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      searchStr: ""
-    };
-    this.handleSearch = this.handleSearch.bind(this);
-    this.renderSearchResults = this.renderSearchResults.bind(this);
-    this.toggleFollow = this.toggleFollow.bind(this);
-    this.followButtonText = this.followButtonText.bind(this);
   }
 
   componentDidMount() {
@@ -29,85 +22,12 @@ export default class Notifications extends Component {
     };
     this.props.getNotes(data);
   }
-
-  handleSearch(text) {
-    this.setState({ searchStr: text }, (search) => {
-      if(this.state.searchStr === "") {
-        this.props.clearResults();
-      } else {
-        this.props.requestSearch(this.state.searchStr);
-      }
-    });
-  }
-
-  renderSearchResults() {
-    if (this.props.searchResults.length > 0) {
-      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-      let source = ds.cloneWithRows(this.props.searchResults);
-      return (
-        <View>
-          <ListView
-            dataSource={source}
-            renderRow={(rowData) =>
-              <View>
-                <Text>
-                  {rowData.username}
-                </Text>
-                <TouchableOpacity onPress={() => this.toggleFollow(rowData.id)}>
-                  <Text>{this.followButtonText(rowData.id)}</Text>
-                </TouchableOpacity>
-              </View>
-            }
-          />
-        </View>
-      );
-    }
-  }
-
-  toggleFollow(id) {
-    const follow = {
-      follower_id: this.props.currentUser.id,
-      leader_id: id
-    };
-    if (this.following(id)) {
-      this.props.unfollow(follow);
-    } else {
-      this.props.follow(follow);
-    }
-  }
-
-  following(id) {
-    if (this.props.currentUser.leaders[id]) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  followButtonText(id) {
-    if (this.following(id)) {
-      return 'Following';
-    } else {
-      return 'Follow';
-    }
-  }
-
   render() {
     return (
       <View>
         <Text>
           Notifications
         </Text>
-        <View>
-          <TextInput
-            style={baseStyles.input}
-            placeholder='Search Users'
-            onChangeText={(text) => this.handleSearch(text)}
-          />
-        </View>
-        <View>
-          {this.renderSearchResults()}
-        </View>
       </View>
     );
   }
--- app/components/screens/notifications/notifications_container.js
+++ app/components/screens/notifications/notifications_container.js
@@ -3,15 +3,11 @@ import { connect } from 'react-redux';
 import Notifications from './notifications';
 
 import { receiveLocation, receiveGooglePlaces } from '../../../actions/location_actions';
-import { requestSearch, clearSearchResults } from '../../../actions/search_users_actions';
-import { selectSearchResults } from '../../../reducers/selectors';
-import { requestFollow, requestUnfollow } from '../../../actions/follows_actions';
 import { requestAllNotes } from '../../../actions/note_actions';
 
 const mapStateToProps = ( state ) => {
   return {
     location: state.location,
-    searchResults: selectSearchResults(state),
     currentUser: state.session.currentUser,
     notes: state.notes
   };
@@ -20,10 +16,6 @@ const mapStateToProps = ( state ) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     receiveLocation: location => dispatch(receiveLocation(location)),
-    requestSearch: searchStr => dispatch(requestSearch(searchStr)),
-    clearResults: () => dispatch(clearSearchResults()),
-    follow: follow => dispatch(requestFollow(follow)),
-    unfollow: follow => dispatch(requestUnfollow(follow)),
     getNotes: locations => dispatch(requestAllNotes(locations))
   };
 };
