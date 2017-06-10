5b911fae56f9a7d332070371ae91c197294e6e23
Monte Jiran <montejiran@gmail.com>
2017-06-10 11:35:16 -0700

    add search to discover

--- app/components/screens/feed/public_feed.js
+++ app/components/screens/feed/public_feed.js
@@ -5,7 +5,9 @@ import {
   Text,
   Image,
   ScrollView,
+  ListView,
   Dimensions,
+  TextInput,
   View,
   FlatList
 } from 'react-native';
@@ -24,11 +26,84 @@ export default class PublicFeed extends Component {
     this.props.getFeed(this.props.location.places_nearby);
   }
 
+  handleSearch(text) {
+    this.setState({ searchStr: text }, (search) => {
+      if(this.state.searchStr === "") {
+        this.props.clearResults();
+      } else {
+        this.props.requestSearch(this.state.searchStr);
+      }
+    });
+  }
+
+  renderSearchResults() {
+    if (this.props.searchResults.length > 0) {
+      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+      let source = ds.cloneWithRows(this.props.searchResults);
+      return (
+        <View>
+          <ListView
+            dataSource={source}
+            renderRow={(rowData) =>
+              <View>
+                <Text>
+                  {rowData.username}
+                </Text>
+                <TouchableOpacity onPress={() => this.toggleFollow(rowData.id)}>
+                  <Text>{this.followButtonText(rowData.id)}</Text>
+                </TouchableOpacity>
+              </View>
+            }
+          />
+        </View>
+      );
+    }
+  }
+
+  toggleFollow(id) {
+    const follow = {
+      follower_id: this.props.currentUser.id,
+      leader_id: id
+    };
+    if (this.following(id)) {
+      this.props.unfollow(follow);
+    } else {
+      this.props.follow(follow);
+    }
+  }
+
+  following(id) {
+    if (this.props.currentUser.leaders[id]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  followButtonText(id) {
+    if (this.following(id)) {
+      return 'Following';
+    } else {
+      return 'Follow';
+    }
+  }
+
+
   render() {
-    let closest = this.props.location.places_nearby[0]
+    let closest = this.props.location.places_nearby[0];
     return (
       <View style={baseStyles.screen} >
         <View>
+          <View>
+            <TextInput
+              style={baseStyles.input}
+              placeholder='Search Users'
+              onChangeText={(text) => this.handleSearch(text)}
+            />
+          </View>
+          <View>
+            {this.renderSearchResults()}
+          </View>
           <Text style={baseStyles.headlineTwo}>What's happening now at</Text>
           <Text style={baseStyles.headlines}>{this.props.location.places_nearby[0]}</Text>
 
@@ -37,11 +112,11 @@ export default class PublicFeed extends Component {
             renderItem={({ item }) => (
               <PostContainer post={item} />
             )}
-            keyExtractor={item => item.id} 
+            keyExtractor={item => item.id}
             removeClippedSubviews={false}
              />
 
-        </View>       
+        </View>
       </View>
     );
   }
