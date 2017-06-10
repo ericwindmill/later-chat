4ecb5bba0558b97ac0758739ce79fb58f29aed13
Monte Jiran <montejiran@gmail.com>
2017-06-10 11:32:09 -0700

    add props to public feed

--- app/components/screens/feed/public_feed_container.js
+++ app/components/screens/feed/public_feed_container.js
@@ -1,16 +1,24 @@
 import { connect } from 'react-redux';
 import { requestAllPosts } from '../../../actions/posts_actions';
+import { requestSearch, clearSearchResults } from '../../../actions/search_users_actions';
+import { selectSearchResults } from '../../../reducers/selectors';
+import { requestFollow, requestUnfollow } from '../../../actions/follows_actions';
 import PublicFeed from './public_feed';
 
 const mapStateToProps = (state) => ({
   public: '',
   publicPosts: state.posts,
   currentUser: state.session.currentUser,
+  searchResults: selectSearchResults(state),
   location: state.location
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  getFeed: locations => dispatch(requestAllPosts(locations))
+  getFeed: locations => dispatch(requestAllPosts(locations)),
+  requestSearch: searchStr => dispatch(requestSearch(searchStr)),
+  clearResults: () => dispatch(clearSearchResults()),
+  follow: follow => dispatch(requestFollow(follow)),
+  unfollow: follow => dispatch(requestUnfollow(follow)),
 });
 
 export default connect(
