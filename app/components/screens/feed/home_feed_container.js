a14ce99c5958e5557fbb29cd911ca130f2eb5e6f
Monte Jiran <montejiran@gmail.com>
2017-06-10 11:03:15 -0700

    add semicolons to erics code

--- app/components/screens/feed/home_feed_container.js
+++ app/components/screens/feed/home_feed_container.js
@@ -6,14 +6,14 @@ import HomeFeed from './home_feed';
 // TODO: make sure this is passing the array of followers and the posts correctly, once we set up that slice of state
 const mapStateToProps = (state) => {
 
-  let leaders = []
-  Object.keys(state.session.currentUser.leaders).forEach((leaderId) => leaders.push(parseInt(leaderId)))
+  let leaders = [];
+  Object.keys(state.session.currentUser.leaders).forEach((leaderId) => leaders.push(parseInt(leaderId)));
   return {
     home: '',
     posts: followedPosts(state.posts, leaders),
     currentUser: state.session.currentUser,
     location: state.location
-  }
+  };
 };
 
 const mapDispatchToProps = (dispatch) => ({
