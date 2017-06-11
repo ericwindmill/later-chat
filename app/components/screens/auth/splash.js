a0d5434882d6d803981ba425f1b28d9d3874dcd1
Monte Jiran <montejiran@gmail.com>
2017-06-11 12:27:19 -0700

    can log in, no public feed

--- app/components/screens/auth/splash.js
+++ app/components/screens/auth/splash.js
@@ -50,7 +50,6 @@ export default class SplashScreen extends Component {
       if (response.status >= 200 && response.status < 300) {
         let userSession = await fetch('https://later-chat.herokuapp.com/api/session?auth_token%5D='+token);
         let sessionRes = await userSession.json();
-
         this.props.receiveCurrentUser(sessionRes);
         //Verified token means user is logged in so we redirect him to home.
         this.props.navigation.navigate('Tabs');
--- app/components/screens/feed/home_feed_container.js
+++ app/components/screens/feed/home_feed_container.js
@@ -4,8 +4,9 @@ import { followedPosts } from '../../../reducers/selectors';
 import HomeFeed from './home_feed';
 
 const mapStateToProps = (state) => {
-  let leaders = Object.keys(state.session.currentUser.leaders);
-  if (leaders.length > 0) {
+  debugger;
+  if (state.session.currentUser.leaders) {
+    let leaders = Object.keys(state.session.currentUser.leaders);
     leaders.forEach((leaderId) => leaders.push(parseInt(leaderId)));
     return {
       posts: followedPosts(state.posts, leaders),
