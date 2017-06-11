932a01d5a9302e773ab999b23699ef64c72f2fa9
npl22 <nli22@jhu.edu>
2017-06-11 13:02:36 -0700

    Add demo map view

--- app/components/screens/map/map.js
+++ app/components/screens/map/map.js
@@ -8,11 +8,28 @@ import {
 export default class MapScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>
-          Map
-        </Text>
-      </View>
+      <MapView
+        style={styles.container}
+        provider={'google'}
+        initialRegion={{
+          latitude: 37.7596168,
+          longitude: -122.4279981,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation={true}
+        >
+        <MapView.Marker
+          coordinate={{ latitude: 37.759782, longitude: -122.427093 }}
+          title={'Dolores Park'}
+          description={'Locals of all ages gather at this outdoor hot spot for picnics & people-watching.'}
+        />
+        <MapView.Marker
+          coordinate={{ latitude: 37.761356, longitude: -122.425806 }}
+          title={'Dolores Park Cafe'}
+          description={'Casual cafe serving organic food & drinks with a spacious deck across from Dolores Park.'}
+        />
+      </MapView>
     );
   }
 }
